32544fc34976202daf51c10ffdcd392b5c8e70d6
PhArObiS <neil.lopes.79@gmail.com>
2023-10-12 18:16:43 -0400

    initial upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+ 
+import { HomeComponent } from './home/home.component'; // added this
+import { LoginComponent } from './login/login.component'; // added this
+import { CustomersComponent } from './customers/customers.component'; // added this
+import { OopsComponent } from './oops/oops.component'; // added this
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent}, // added this
+  {path: 'login', component: LoginComponent}, // added this
+  {path: 'customers', component: CustomersComponent}, // added this
+  {path: '**', component: OopsComponent} // added this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
